f29dee157c9f2ef500d81d627b9ace6988323aa9
Newgitweb <newgit@oxcytech.com>
2024-12-12 20:55:13 +0530

    added image logic for trade module

--- src/controllers/tradeController.js
+++ src/controllers/tradeController.js
@@ -11,20 +11,69 @@ exports.getAllTradeStock = async (req, res) => {
 
 exports.createTradeStock = async (req, res) => {
   try {
-    const {
-      goatName,
-      purchaserInformation,
-      customerDetails,
-      weight,
-      height,
-      entryDate,
-      exitDate,
-      salesStatus,
-      maintenanceRecords,
-      price,
-      description,
-      uniqueIdentificationNumber,
-    } = req.body;
+    const { fields, files } = await parseForm(req);
+
+    // Extract and handle fields
+    const goatName = Array.isArray(fields.goatName)
+      ? fields.goatName[0]
+      : fields.goatName;
+    const purchaserInformation = Array.isArray(fields.purchaserInformation)
+      ? fields.purchaserInformation[0]
+      : fields.purchaserInformation;
+    const customerDetails = Array.isArray(fields.customerDetails)
+      ? fields.customerDetails[0]
+      : fields.customerDetails;
+    const weight = Array.isArray(fields.weight)
+      ? parseFloat(fields.weight[0])
+      : parseFloat(fields.weight);
+    const height = Array.isArray(fields.height)
+      ? parseFloat(fields.height[0])
+      : parseFloat(fields.height);
+    const entryDate = Array.isArray(fields.entryDate)
+      ? new Date(fields.entryDate[0])
+      : new Date(fields.entryDate);
+    const exitDate = Array.isArray(fields.exitDate)
+      ? new Date(fields.exitDate[0])
+      : new Date(fields.exitDate);
+    const salesStatus = Array.isArray(fields.salesStatus)
+      ? fields.salesStatus[0]
+      : fields.salesStatus;
+    const maintenanceRecords = Array.isArray(fields.maintenanceRecords)
+      ? fields.maintenanceRecords[0]
+      : fields.maintenanceRecords;
+    const price = Array.isArray(fields.price)
+      ? parseFloat(fields.price[0])
+      : parseFloat(fields.price);
+    const description = Array.isArray(fields.description)
+      ? fields.description[0]
+      : fields.description;
+    const uniqueIdentificationNumber = Array.isArray(
+      fields.uniqueIdentificationNumber
+    )
+      ? fields.uniqueIdentificationNumber[0]
+      : fields.uniqueIdentificationNumber;
+
+    // Extract and handle image file
+    const imageFile = Array.isArray(files.image) ? files.image[0] : files.image;
+    let imageUrl = null;
+
+    if (imageFile) {
+      const filenameParts = imageFile.originalFilename.split(".");
+      const fileExtension = filenameParts[filenameParts.length - 1];
+
+      // Read the image file buffer
+      const imageBuffer = fs.readFileSync(imageFile.filepath);
+
+      // Upload the image to Vercel Blob storage
+      const blob = await put(
+        `${uniqueIdentificationNumber}.${fileExtension}`,
+        imageBuffer,
+        { access: "public" }
+      );
+
+      // Save the public URL of the image
+      imageUrl = blob.url;
+    }
 
     const newTradeStock = await prisma.tradeStock.create({
       data: {
@@ -40,6 +89,7 @@ exports.createTradeStock = async (req, res) => {
         price,
         description,
         uniqueIdentificationNumber,
+        photographUrl: imageUrl,
       },
     });
     res.status(201).json(newTradeStock);
@@ -54,38 +104,123 @@ exports.createTradeStock = async (req, res) => {
 exports.updateTradeStock = async (req, res) => {
   try {
     const { id } = req.params;
-    const {
-      goatName,
-      purchaserInformation,
-      customerDetails,
-      weight,
-      height,
-      entryDate,
-      exitDate,
-      salesStatus,
-      maintenanceRecords,
-      price,
-      description,
-      uniqueIdentificationNumber,
-    } = req.body;
 
-    const updatedTradeStock = await prisma.tradeStock.update({
-      where: { id: parseInt(id) },
-      data: {
-        goatName,
-        purchaserInformation,
-        customerDetails,
-        weight,
-        height,
-        entryDate: entryDate ? new Date(entryDate) : null, // Convert to Date object
-        exitDate: exitDate ? new Date(exitDate) : null, // Convert to Date object
-        salesStatus,
-        maintenanceRecords,
-        price,
-        description,
-        uniqueIdentificationNumber,
-      },
-    });
+    const { fields, files } = await parseForm(req);
+
+    let updatedTradeStock;
+
+    // Extract and handle fields
+    const goatName = Array.isArray(fields.goatName)
+      ? fields.goatName[0]
+      : fields.goatName;
+    console.log("goatName", goatName);
+    const purchaserInformation = Array.isArray(fields.purchaserInformation)
+      ? fields.purchaserInformation[0]
+      : fields.purchaserInformation;
+    const customerDetails = Array.isArray(fields.customerDetails)
+      ? fields.customerDetails[0]
+      : fields.customerDetails;
+    const weight = Array.isArray(fields.weight)
+      ? parseFloat(fields.weight[0])
+      : parseFloat(fields.weight);
+    const height = Array.isArray(fields.height)
+      ? parseFloat(fields.height[0])
+      : parseFloat(fields.height);
+    const entryDate = Array.isArray(fields.entryDate)
+      ? new Date(fields.entryDate[0])
+      : new Date(fields.entryDate);
+    const exitDate = Array.isArray(fields.exitDate)
+      ? new Date(fields.exitDate[0])
+      : new Date(fields.exitDate);
+    const salesStatus = Array.isArray(fields.salesStatus)
+      ? fields.salesStatus[0]
+      : fields.salesStatus;
+    const maintenanceRecords = Array.isArray(fields.maintenanceRecords)
+      ? fields.maintenanceRecords[0]
+      : fields.maintenanceRecords;
+    const price = Array.isArray(fields.price)
+      ? parseFloat(fields.price[0])
+      : parseFloat(fields.price);
+    const description = Array.isArray(fields.description)
+      ? fields.description[0]
+      : fields.description;
+    const uniqueIdentificationNumber = Array.isArray(
+      fields.uniqueIdentificationNumber
+    )
+      ? fields.uniqueIdentificationNumber[0]
+      : fields.uniqueIdentificationNumber;
+
+    // Extract and handle image file
+    const imageFile = Array.isArray(files.image) ? files.image[0] : files.image;
+    let imageUrl = null;
+
+    if (imageFile) {
+      const tradeStock2 = await prisma.tradeStock.findFirst({
+        where: { id: parseInt(id) },
+      });
+
+      await deleteBlogImage(tradeStock2.photographUrl);
+
+      console.log("imageFile", imageFile);
+      const filenameParts = imageFile.originalFilename.split(".");
+      const fileExtension = filenameParts[filenameParts.length - 1];
+
+      // Read the image file buffer
+      const imageBuffer = fs.readFileSync(imageFile.filepath);
+
+      console.log("imageBuffer", imageBuffer);
+
+      const time = Math.floor(Date.now() / 1000);
+
+      // Upload the image to Vercel Blob storage
+      const blob = await put(
+        `${uniqueIdentificationNumber}_${time}.${fileExtension}`,
+        imageBuffer,
+        { access: "public" }
+      );
+
+      console.log("blob", blob);
+
+      // Save the public URL of the image
+      imageUrl = blob.url;
+
+      updatedTradeStock = await prisma.tradeStock.update({
+        where: { id: parseInt(id) },
+        data: {
+          goatName,
+          purchaserInformation,
+          customerDetails,
+          weight,
+          height,
+          entryDate: entryDate ? new Date(entryDate) : null, // Convert to Date object
+          exitDate: exitDate ? new Date(exitDate) : null, // Convert to Date object
+          salesStatus,
+          maintenanceRecords,
+          price,
+          description,
+          uniqueIdentificationNumber,
+          photographUrl: imageUrl,
+        },
+      });
+    } else {
+      updatedTradeStock = await prisma.tradeStock.update({
+        where: { id: parseInt(id) },
+        data: {
+          goatName,
+          purchaserInformation,
+          customerDetails,
+          weight,
+          height,
+          entryDate: entryDate ? new Date(entryDate) : null, // Convert to Date object
+          exitDate: exitDate ? new Date(exitDate) : null, // Convert to Date object
+          salesStatus,
+          maintenanceRecords,
+          price,
+          description,
+          uniqueIdentificationNumber,
+        },
+      });
+    }
     res.json(updatedTradeStock);
   } catch (error) {
     console.error("Error updating trade stock:", error);
